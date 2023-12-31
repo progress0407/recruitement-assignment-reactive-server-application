819b8bd15106bb5384f769ffa05bcb02f43faee1
progress0407 <progress0407@gmail.com>
2023-12-31 17:49:49 +0900

    1. mockk 라이브러리 추가  2. Item 통합 테스트 재 작성 (to Green)

--- src/test/kotlin/io/philo/item/ItemIntegrationTest.kt
+++ src/test/kotlin/io/philo/item/ItemIntegrationTest.kt
@@ -1,25 +1,47 @@
 package io.philo.item
 
+import com.ninjasquad.springmockk.MockkBean
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.shouldNotBe
+import io.mockk.every
+import io.philo.auth.JwtManager
 import io.philo.dto.ResourceCreateResponse
 import io.philo.shop.dto.ItemCreateRequest
 import io.philo.shop.dto.ItemListResponse
 import io.philo.shop.entity.Item
 import io.philo.support.IntegrationTest
 import org.junit.jupiter.api.Test
+import org.springframework.http.HttpHeaders.AUTHORIZATION
 import org.springframework.http.MediaType.APPLICATION_JSON
 
+
 class ItemIntegrationTest: IntegrationTest() {
+
+    @MockkBean
+    lateinit var jwtManager: JwtManager
 
     @Test
     fun `재고 등록 및 조회`() {
 
-        webTestClient.post().uri("/items")
+        every { jwtManager.isValidToken(any()) } returns true
+        every { jwtManager.parse(any()) } returns "1"
+
+
+        val createdDto = webTestClient.post().uri("/items")
+            .headers { headers ->
+                headers.add(AUTHORIZATION, "access token here")
+                headers.add("userId", "1")
+            }
             .contentType(APPLICATION_JSON)
             .bodyValue(ItemCreateRequest.fixture)
             .exchange()
             .expectStatus().isCreated
             .expectBody(ResourceCreateResponse::class.java)
-            .isEqualTo(ResourceCreateResponse(1L))
+            .returnResult()
+            .responseBody
+
+        createdDto shouldNotBe null
+        (createdDto!!.id > 0L) shouldBe true
 
         webTestClient.get().uri("/items")
             .exchange()
